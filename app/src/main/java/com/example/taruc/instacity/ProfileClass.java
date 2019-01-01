79a4a92cc76935d5c221bf743ebbc50937270693
GORDON9839 <gohcl-aa16@student.tarc.edu.my>
2019-01-01 21:37:02 +0800

    update 2

---
+++ app/src/main/java/com/example/taruc/instacity/ProfileClass.java
@@ -0,0 +1,20 @@
+package com.example.taruc.instacity;
+
+public class ProfileClass {
+    private String postImage;
+
+    public ProfileClass(String postImage) {
+        this.postImage = postImage;
+    }
+
+    public String getpostImage() {
+        return postImage;
+    }
+
+    public void setpostImage(String postImage) {
+        this.postImage = postImage;
+    }
+
+    public ProfileClass() {
+    }
+}
